cdeba76908dcde2bd37647136c61e0e8fcf70b5c
abiodun <stepheng323@gmail.com>
2021-09-27 13:20:42 +0100

    add env

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const apiRouter =  require('./routes');
+const { respondWithSuccess } = require('./helpers/responseHandler');
 
 const app = express();
 
 app.use(express.json())
 app.use(cors())
 app.use(apiRouter);
+app.get('/', (req, res) => respondWithSuccess(res, 200, 'Welcome to star wars api'));
+
 
 const PORT = 3000;
 
